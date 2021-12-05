9e8cb21e327127a4a7d43e5036cb98f5a0bd4d53
Andrew Reid <andrew.ce.reid@gmail.com>
2021-12-05 14:57:37 -0500

    Rest of day 3.

--- day03/day03b.py
+++ day03/day03b.py
@@ -1,22 +1,28 @@
 
 inc = {'0':-1,'1':1}
-val = {True: 1, False: 0}
-dct = {}
+val = {True: '1', False: '0'}
 oxy = set()
 co2 = set()
 
-nbits = 5
-filename = "test.txt"
+nbits = 12
+filename = "input.txt"
 
 # Probably memory-inefficient, but here goes.
 # Retain every element in the set whose bit at
 # position bitpos (counting from left to right)
 # matches the template. 
-def prune(inset,bitpos,template):
+def prune(inset,bitpos,sense):
+    mci = 0 # Most common indicator
+    for s in inset:
+        mci += inc[s[bitpos]]
     res = set()
     for s in inset:
-        if int(s[bitpos])==template[bitpos]:
-            res.add(s)
+        if sense=='+':
+            if val[mci>=0]==s[bitpos]:
+                res.add(s)
+        elif sense=='-':
+            if val[mci>=0]!=s[bitpos]:
+                res.add(s)
     return res
     
 if __name__=="__main__":
@@ -24,32 +30,24 @@ if __name__=="__main__":
     for l in f:
         oxy.add(l[:-1])
         co2.add(l[:-1])
-        for c in zip(range(nbits,0,-1),l[:-1]):
-            delta = inc[c[1]]
-            try:
-                dct[c[0]]+=delta
-            except KeyError:
-                dct[c[0]]=delta
 
-    # For bit position k-1 (MSB is 11, LSB is 0), the most common
-    # bit is 1 if dct[k]>0, and zero of dct[k]<0.
-
-    # TODO: This is wrong, you need to re-assess the most
-    # common bit after each pruning operation. 
-    oxy_template = [ val[ dct[k] > 0] for k in range(nbits,0,-1)]
     for pos in range(nbits):
-        oxy = prune(oxy,pos,oxy_template)
+        oxy = prune(oxy,pos,'+')
         if len(oxy)==1:
             break
 
-    co2_template = [ val[ dct[k] < 0] for k in range(nbits,0,-1)]
     for pos in range(nbits):
-        co2 = prune(co2,pos,co2_template)
+        co2 = prune(co2,pos,'-')
         if len(co2)==1:
             break
 
-    print(oxy,co2)
-        
+    # Can't index into sets, but they're unary, so
+    # iteration is cheap.
+    for so in oxy:
+        vo = int(so,2)
+        for sc in co2:
+            vc = int(sc,2)
+            print("Product: ",vo*vc)
 
     
             
