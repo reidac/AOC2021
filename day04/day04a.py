5339729efe98339abf8b885a97c9f715574475ac
Andrew Reid <andrew.ce.reid@gmail.com>
2021-12-05 17:46:18 -0500

    Day 4 part 1.

--- day04/day04a.py
+++ day04/day04a.py
@@ -12,9 +12,32 @@ class Board:
                 self.data[ v[1] ] = [ (self.rows,v[0]) ]
         self.rows += 1
     def select(self,val):
-        pass
+        if val in self.data.keys():
+            self.selections.add(val)
     def win(self):
-        pass
+        row_data = {}
+        col_data = {}
+        for s in self.selections:
+            for rc in self.data[s]:
+                (r,c) = rc
+                try:
+                    row_data[r]+=1
+                except KeyError:
+                    row_data[r]=1
+                try:
+                    col_data[c]+=1
+                except KeyError:
+                    col_data[c]=1
+        if ( 5 in row_data.values()) or (5 in col_data.values() ):
+            return True
+        return False
+    def score(self,lastval):
+        # Sum of all the unselected numbers, times the last value.
+        sum = 0
+        for v in self.data.keys():
+            if not (v in self.selections):
+                sum += v
+        return sum*lastval
     def __repr__(self):
         # Representation is inefficient but also infrequent.
         res = ''
@@ -46,5 +69,15 @@ if __name__=="__main__":
            vs = [ int(x) for x in l.split() ]
            b.addrow(vs)
 
-    for b in boards:
-        print(b)
+    done = False
+    for d in draws:
+        for b in boards:
+            b.select(d)
+            if (b.win()):
+                print("First winning board's score is ", b.score(d))
+                done = True
+                break
+        if (done):
+            break
+
+
