81d88c5afa28dd5ef21b27a236740f04fcf08151
Andrew Reid <andrew.ce.reid@gmail.com>
2021-12-11 14:15:41 -0500

    Day 11. So many off-by-ones...

---
+++ day11/day11b.py
@@ -0,0 +1,72 @@
+
+filename = "input.txt"
+# filename = "special.txt"
+# filename = "test.txt"
+
+adjacencies = [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)]
+
+def increment(grid):
+    for k in grid.keys():
+        grid[k]+=1
+    res = 0
+    done = False
+    flashed = set()
+    while not done:
+        delta = 0
+        for k in grid.keys():
+            if (not (k in flashed)) and (grid[k]>9):
+                delta += 1
+                flashed.add(k)
+                for a in adjacencies:
+                    try:
+                        grid[(k[0]+a[0],k[1]+a[1])] += 1
+                    except KeyError:
+                        pass
+        if (delta==0):
+            done = True
+        else:
+            res += delta
+
+    for k in flashed:
+        grid[k]=0
+            
+    return res
+
+def draw(grid,rows,cols):
+    for c in range(cols):
+        ln = ""
+        for r in range(rows):
+            ln += '{0:3d}'.format(grid[r,c])
+        print(ln)
+                       
+    
+if __name__=="__main__":
+    grid = {}
+    f = open(filename,"r")
+    col_index = 0
+    for l in f:
+        row_index = 0
+        for ch in l[:-1]:
+            grid[row_index,col_index] = int(ch)
+            row_index += 1
+        col_index += 1
+
+    sum = 0
+    count = 0
+    failed = False
+    done = False
+    while not done:
+        count += 1
+        delta = increment(grid)
+        if (delta==100):
+            done = True
+        if count > 10000:
+            done = True
+            failed = True
+
+    if not failed:
+        print("Synchronization on step ", count)
+    else:
+        print("Failed after ", count, " steps.")
+            
+    
